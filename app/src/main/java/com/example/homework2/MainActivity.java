f86c8fab6976650e2b03b318549ad518096e091c
artur.lozovyi <lozoviy.artur@gmail.com>
2021-11-14 21:48:10 +0200

    HW3

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -3,12 +3,130 @@ package com.example.homework2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private CalculationData mCalculationData = new CalculationData();
+    private Numbers mCounters;
+
+    private TextView mInputField;  // пользовательский элемент 1-го счетчика
+    private TextView textCounter1;  // пользовательский элемент 2-го счетчика
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCounters = new Numbers();
+        initView();
+
+
+
+    }
+
+    private void initView() {
+        // Получить пользовательские элементы по идентификатору
+        mInputField = findViewById(R.id.calcField);
+    }
+
+    private void initButton2ClickListener() {
+        Button button0 = findViewById(R.id.digit_0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounters.incrementCounter0();
+                setTextCounter(mInputField, mCounters.getCounter0());
+            }
+        });
+    }
+
+    private void setTextCounter(TextView textCounter, int counter) {
+        textCounter.setText(String.format(Locale.getDefault(), "%d", counter));
+    }
+
+    public void processOnClick(View v) {
+        final int id = v.getId();
+        switch (id) {
+            case R.id.digit_0:
+            case R.id.digit_1:
+            case R.id.digit_2:
+            case R.id.digit_3:
+            case R.id.digit_4:
+            case R.id.digit_5:
+            case R.id.digit_6:
+            case R.id.digit_7:
+            case R.id.digit_8:
+            case R.id.digit_9:{
+                if(mCalculationData.firstArg == null){
+                    mCalculationData.firstArg= "";
+                    mInputField.setText("");
+                }
+                if(mCalculationData.secondArg == null){
+                    mCalculationData.firstArg += ((Button)v).getText();
+                } else {
+                    mCalculationData.secondArg += ((Button)v).getText();
+                }
+                mInputField.setText(mInputField.getText().toString() + ((Button)v).getText().toString());
+                break;
+            }
+            case R.id.plus:
+            case R.id.minus:
+            case R.id.multiply:
+            case R.id.divide:{
+                mCalculationData.secondArg = "";
+                mCalculationData.operation = ((Button)v).getText().toString();
+                mInputField.setText(mInputField.getText().toString() + ((Button)v).getText().toString());
+                break;
+            }
+
+
+            case R.id.equal: {
+                final int result = calculate();
+                mInputField.setText(""+ result);
+                clearData();
+
+                break;
+            }
+        }
+    }
+
+    private int calculate() {
+        final int first = Integer.parseInt(mCalculationData.firstArg);
+        final int second =  Integer.parseInt(mCalculationData.secondArg);
+        switch (mCalculationData.operation){
+            case "-":
+                return first - second;
+            case "+":
+                return first + second;
+            case "%":
+                return first % second;
+            case "*":
+                return first * second;
+            default:
+                return -1;
+
+
+        }
+    }
+
+    private class CalculationData{
+        String firstArg;
+        String secondArg;
+        String operation;
+
+    }
+
+    private void clearData(){
+        mCalculationData.firstArg = null;
+        mCalculationData.secondArg = null;
+        mCalculationData.operation = null;
     }
 }
+
---
+++ app/src/main/java/com/example/homework2/Numbers.java
@@ -0,0 +1,54 @@
+package com.example.homework2;
+
+public class Numbers {
+    private int counter0;
+    private int counter1;
+    private int counter2;
+    private int counter3;
+    private int counter4;
+    private int counter5;
+    private int counter6;
+    private int counter7;
+    private int counter8;
+    private int counter9;
+    private int counterPlus;
+    private int counterMinus;
+    private int counterEqual;
+    private int counterMultiply;
+    private int counterDivide;
+    private int counterPoint;
+    private int counterClear;
+
+
+    public Numbers(){
+        counter0 = 0;
+        counter1 = 0;
+        counter2 = 0;
+        counter3 = 0;
+        counter4 = 0;
+        counter5 = 0;
+        counter6 = 0;
+        counter7 = 0;
+        counter8 = 0;
+        counter9 = 0;
+        counterPlus = 0;
+        counterPoint = 0;
+        counterPlus = 0;
+        counterMultiply = 0;
+        counterMinus = 0;
+        counterEqual = 0;
+        counterDivide = 0;
+        counterClear = 0;
+    }
+
+    public void incrementCounter0(){
+        counter0++;
+    }
+
+    public int getCounter0() {
+        return counter0;
+    }
+
+
+
+}
